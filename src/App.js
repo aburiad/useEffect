86d84fd9f1de0d3f954803c7b2efd6eb0b44b29a
Riad <aburiad507@gmail.com>
2022-06-01 19:46:38 +0600

    version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>User Data from JSON-Placeholder</h2>
+      <h4>using useState and useEffect</h4>
+      <DataComponent></DataComponent>
     </div>
   );
 }
+
+function DataComponent(){
+  const [user,setUser] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(data=>setUser(data));
+  },[])
+  const userStyle = {
+    backgroundColor:'black',color:'white',padding:'30px',margin:'10px',display:'inline-block',boxSizing:'border-box',width: '300px'
+  }
+  return(
+    <div>
+      {
+        user.map((userdata)=>{
+          const {name,email,address,phone} = userdata
+          return(
+            <div style={userStyle}>
+             <h2>{name}</h2>
+            <h4>{email}</h4>
+            <h4>{address.city}</h4>
+            <h4>{phone}</h4>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
 
 export default App;
